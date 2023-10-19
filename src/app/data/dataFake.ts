8ff081ada857adb675bd1b20017d136de201d015
Raissa Peneluc <raissapeneluc@gmail.com>
2023-10-19 13:26:39 -0300

    content created

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,36 @@
+export const dataFake = [
+  {
+    "id": "1",
+    "title": "Último Filme da Saga Harry Potter",
+    "description": "O último filme da saga Harry Potter foi lançado, deixando os potterheads tristes por não terem mais contéudos e filmes desse mundo mágicos.",
+    "photoCover": "https://th.bing.com/th/id/OIP.MakwUv_6Myl53xW_no2OmgHaEo?w=252&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7"
+  },
+
+  {
+    "id": "2",
+    "title": "Série de Harry Potter foi confirmada",
+    "description": "A produção da série de Harry Potter, escrita por J.K. Rowling, foi confirmada pela Warner Bros. A nova adaptação da saga, originalmente lançada com sete livros, será para o serviço de streaming Max, novo nome da plataforma que une a HBO Max e a Discovery+.",
+    "photoCover": "https://th.bing.com/th/id/OIP.oOlGl8LAKmjwNcAx0RJGxAHaEK?w=312&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7"
+  },
+
+  {
+    "id": "3",
+    "title": "Harry Potter: Tudo o que sabemos sobre a série do HBO.",
+    "description": "A produção da série de Harry Potter, escrita por J.K. Rowling, foi confirmada pela Warner Bros. A nova adaptação da saga, originalmente lançada com sete livros, será para o serviço de streaming Max, novo nome da plataforma que une a HBO Max e a Discovery+.",
+    "photoCover": "https://th.bing.com/th/id/OIP.5ry13dhluQWdY_PoxhfNiQHaDt?w=315&h=174&c=7&r=0&o=5&dpr=1.3&pid=1.7"
+  },
+
+  {
+    "id": "4",
+    "title": "Quantos livros tem a série Harry Potter e qual a ordem de ler?",
+    "description": "A produção da série de Harry Potter, escrita por J.K. Rowling, foi confirmada pela Warner Bros. A nova adaptação da saga, originalmente lançada com sete livros, será para o serviço de streaming Max, novo nome da plataforma que une a HBO Max e a Discovery+.",
+    "photoCover": "https://th.bing.com/th/id/OIP.QcMH_Fn7iyBk2oTZhAvaEAHaFj?w=212&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7"
+  },
+
+  {
+    "id": "5",
+    "title": "Qual a história de Severus Snape?",
+    "description": "A produção da série de Harry Potter, escrita por J.K. Rowling, foi confirmada pela Warner Bros. A nova adaptação da saga, originalmente lançada com sete livros, será para o serviço de streaming Max, novo nome da plataforma que une a HBO Max e a Discovery+.",
+    "photoCover": "https://th.bing.com/th/id/OIP.Hyk1OUQ-VN3Cy_T5T_lCcAHaDt?w=335&h=175&c=7&r=0&o=5&dpr=1.3&pid=1.7"
+  },
+]
