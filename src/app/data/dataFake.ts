7b7c0552811038500e7d3c88586935e1ed66e4c1
Raissa Peneluc <raissapeneluc@gmail.com>
2023-10-19 13:39:06 -0300

    more description

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -16,21 +16,21 @@ export const dataFake = [
   {
     "id": "3",
     "title": "Harry Potter: Tudo o que sabemos sobre a série do HBO.",
-    "description": "A produção da série de Harry Potter, escrita por J.K. Rowling, foi confirmada pela Warner Bros. A nova adaptação da saga, originalmente lançada com sete livros, será para o serviço de streaming Max, novo nome da plataforma que une a HBO Max e a Discovery+.",
+    "description": "Essa nova adaptação terá produção executiva da autora da saga, J.K. Rowling. Ainda é incerto se ela terá papel ativo na produção, ou se é apenas um cargo simbólico. O tamanho e o escopo da série do HBO Max de Harry Potter provavelmente indicam que a rede ainda está a alguns anos de distância de uma estreia. Não houve nenhum anúncio de elenco para a série Harry Potter da HBO Max, mas certamente haverá muita especulação sobre quem viverá os personagens queridos. Os atores principais, Daniel Radcliffe, Emma Watson e Rupert Grint não retornarão aos papéis originais em razão da idade, mas nada impede que eles apareçam em outros papéis, ou até que outros astros reprisem seus papéis na nova série.",
     "photoCover": "https://th.bing.com/th/id/OIP.5ry13dhluQWdY_PoxhfNiQHaDt?w=315&h=174&c=7&r=0&o=5&dpr=1.3&pid=1.7"
   },
 
   {
     "id": "4",
     "title": "Quantos livros tem a série Harry Potter e qual a ordem de ler?",
-    "description": "A produção da série de Harry Potter, escrita por J.K. Rowling, foi confirmada pela Warner Bros. A nova adaptação da saga, originalmente lançada com sete livros, será para o serviço de streaming Max, novo nome da plataforma que une a HBO Max e a Discovery+.",
+    "description": "A série Harry Potter tem 7 livros e a ordem deles são: Harry Potter e a Pedra Filosofal, Harry Potter e a Câmara Secreta, Harry Potter e o Prisioneiro de Azkaban, Harry Potter e o Cálice de Fogo, Harry Potter e a Ordem da Fênix, Harry Potter e o Enigma do Príncipe e Harry Potter e as Relíquias da Morte.",
     "photoCover": "https://th.bing.com/th/id/OIP.QcMH_Fn7iyBk2oTZhAvaEAHaFj?w=212&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7"
   },
 
   {
     "id": "5",
     "title": "Qual a história de Severus Snape?",
-    "description": "A produção da série de Harry Potter, escrita por J.K. Rowling, foi confirmada pela Warner Bros. A nova adaptação da saga, originalmente lançada com sete livros, será para o serviço de streaming Max, novo nome da plataforma que une a HBO Max e a Discovery+.",
+    "description": "Severus Snape foi um bruxo mestiço inglês que serviu como Mestre de Poções, Chefe da Casa Sonserina e Professor de Defesa Contra as Artes das Trevas em Hogwarts. Ele também foi membro da Ordem da Fênix e dos Comensais da Morte. Sua vida dupla desempenhou um papel extremamente importante nas duas Guerras Bruxas contra Voldemort. Severus nasceu em 9 de janeiro de 1960 em Spinner's End, Cokeworth, Midlands, Inglaterra, filho de Tobias Snape e Eileen Prince. Ele conheceu Lily Evans quando tinha nove anos e os dois se tornaram amigos próximos. Severus começou em Hogwarts com Lily em 1971, onde foi selecionado para a Casa Sonserina. Isso o colocou no mesmo ano que Lily, mas infelizmente para ele ela foi selecionada para a Casa Grifinória. Severus se tornou o inimigo imediato de James Potter e Sirius Black e foi vítima frequente de seus bullying.",
     "photoCover": "https://th.bing.com/th/id/OIP.Hyk1OUQ-VN3Cy_T5T_lCcAHaDt?w=335&h=175&c=7&r=0&o=5&dpr=1.3&pid=1.7"
   },
 ]
